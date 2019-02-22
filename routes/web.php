0184ffe1bcbf913d2ce81f76e720908ef2114f6d
andrewakram <36989699+andrewakram@users.noreply.github.com>
2019-02-22 23:39:05 +0200

    2nd commit

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,19 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
+ * ==============================+===============================
+ * Developed By : ANDREW AKRAM ALBERT ZAKI
+ * Contact Me At:
+ * Email : andrewalbert93501@gmail.com
+ * Phone : +2 011 28 5700 38
+ * LinkedIn : https://www.linkedin.com/in/andrew-akram-2167a0154/
+ * ==============================+===============================
+*/
 Route::get('/', function () {
     return view('welcome');
 });
-
-
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
 //=============================================================================
 //================== Admmin Panel routes ======================================
@@ -117,3 +122,12 @@ Route::post('/ar/update/employee/{e_id}',"EmployeeController@update_employee");
 //go to delete_employee
 Route::get('/delete/employee/{e_id}',"EmployeeController@delete_employee");
 Route::get('/ar/delete/employee/{e_id}',"EmployeeController@delete_employee");
+/*
+ * ==============================+===============================
+ * Developed By : ANDREW AKRAM ALBERT ZAKI
+ * Contact Me At:
+ * Email : andrewalbert93501@gmail.com
+ * Phone : +2 011 28 5700 38
+ * LinkedIn : https://www.linkedin.com/in/andrew-akram-2167a0154/
+ * ==============================+===============================
+*/
